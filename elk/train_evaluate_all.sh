351de774a2ba559edfbb82fb542a44dbe1fee0f2
KayKozaronek <kozaronek@gmail.com>
2023-02-17 18:58:50 +0000

    create train_evaluate_all.sh

---
+++ elk/train_evaluate_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for TRAIN in imdb amazon-polarity ag-news dbpedia-14 copa rte boolq qnli piqa
+do
+    CUDA_VISIBLE_DEVICES=7 python -m elk.train --model deberta-v2-xxlarge-mnli --prefix normal  --dataset $TRAIN --num_data 1000 --seed 1
+    for EVAL in imdb amazon-polarity ag-news dbpedia-14 copa rte boolq qnli piqa
+    do
+        CUDA_VISIBLE_DEVICES=7 python -m elk.evaluate --model deberta-v2-xxlarge-mnli --prefix normal --dataset $TRAIN --dataset_eval $EVAL  --num_data 1000 --seed 1
+    done
+done
