d2a563faec4f5af38716cbf587ff28d733b0045c
KayKozaronek <kozaronek@gmail.com>
2023-02-17 18:55:15 +0000

    refactor main.py

--- elk/main.py
+++ elk/main.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 import pickle
 
-from train import train
+from train import train, save_trained_models
 from evaluate import evaluate
 from elk.utils_evaluation.parser import get_args
 from pathlib import Path
@@ -19,32 +19,10 @@ if __name__ == "__main__":
     random.seed(args.seed)
     np.random.seed(args.seed)
 
-    logistic_regression_model, ccs_model = train(args)
+    logistic_regression_model, ccs_model, all_ccs_per_prompt = train(args)
+    save_trained_models(args, logistic_regression_model, ccs_all_prompts, all_ccs_per_prompt)
 
-    # save models
-    # TODO: use better filename for the pkls, so they don't get overwritten
-    with open(args.trained_models_path / "logistic_regression_model.pkl", "wb") as file:
-        pickle.dump(logistic_regression_model, file)
-    torch.save(ccs_model.state_dict(), args.trained_models_path / "ccs_model.pth")
+    evaluate(args, logistic_regression_model, ccs_model, all_ccs_per_prompt)
 
-    evaluate(args, logistic_regression_model, ccs_model)
-
-    evaluation_df = pd.read_csv(args.save_dir / f"{args.model}_{args.prefix}_{args.seed}.csv")
-    evaluation_table = wandb.Table(dataframe=evaluation_df)
-    # Add the table to an Artifact to make it easier to reuse!
-    evaluation_table_artifact = wandb.Artifact("evaluation_artifact", type="dataset")
-    evaluation_table_artifact.add(evaluation_table, "evaluation_table")
-    # We will also log the raw csv file within an artifact to preserve our data
-    evaluation_table_artifact.add_file(args.save_dir / f"{args.model}_{args.prefix}_{args.seed}.csv")
-    # Log the table to visualize with a run...
-    run.log({"evaluation": evaluation_table})
-    # and Log as an Artifact to increase the available row limit!
-    run.log_artifact(evaluation_table_artifact)
-
-    model_artifacts = wandb.Artifact('trained_models', type='model')
-    # model_artifacts.add_dir(args.trained_models_path)
-    model_artifacts.add_file(args.trained_models_path / "ccs_model.pth")
-
-    run.log_artifact(model_artifacts)
     
     
