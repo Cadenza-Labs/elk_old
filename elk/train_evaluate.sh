a07ecfebfdc5fb446dfaaea52627f98f1a1b3980
KayKozaronek <kozaronek@gmail.com>
2023-02-17 19:00:26 +0000

    add project name and seed to train_evaluate and train_evaluate_all

--- elk/train_evaluate.sh
+++ elk/train_evaluate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# python -m elk.train --model deberta-v2-xxlarge-mnli --prefix normal  --dataset imdb  --num_data 1000
-python -m elk.train --model deberta-v2-xxlarge-mnli --prefix normal  --dataset imdb  --num_data 1000
-python -m elk.evaluate --model deberta-v2-xxlarge-mnli --prefix normal  --dataset imdb  --num_data 1000
+CUDA_VISIBLE_DEVICES=7 python -m elk.train --project_name CCS --model deberta-v2-xxlarge-mnli --prefix normal  --dataset imdb --num_data 1000 --seed 1
+CUDA_VISIBLE_DEVICES=7 python -m elk.evaluate --project_name CCS --model deberta-v2-xxlarge-mnli --prefix normal  --dataset imdb --dataset_eval boolq  --num_data 1000 --seed 1
--- elk/train_evaluate_all.sh
+++ elk/train_evaluate_all.sh
@@ -2,9 +2,9 @@
 
 for TRAIN in imdb amazon-polarity ag-news dbpedia-14 copa rte boolq qnli piqa
 do
-    CUDA_VISIBLE_DEVICES=7 python -m elk.train --model deberta-v2-xxlarge-mnli --prefix normal  --dataset $TRAIN --num_data 1000 --seed 1
+    CUDA_VISIBLE_DEVICES=7 python -m elk.train --project_name CCS --model deberta-v2-xxlarge-mnli --prefix normal  --dataset $TRAIN --num_data 1000 --seed 1
     for EVAL in imdb amazon-polarity ag-news dbpedia-14 copa rte boolq qnli piqa
     do
-        CUDA_VISIBLE_DEVICES=7 python -m elk.evaluate --model deberta-v2-xxlarge-mnli --prefix normal --dataset $TRAIN --dataset_eval $EVAL  --num_data 1000 --seed 1
+        CUDA_VISIBLE_DEVICES=7 python -m elk.evaluate --project_name CCS --model deberta-v2-xxlarge-mnli --prefix normal --dataset $TRAIN --dataset_eval $EVAL  --num_data 1000 --seed 1
     done
 done
